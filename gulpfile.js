5298a5a5ea790f38dd04afdac75202232379e9da
lenore <lenorewei@lagou.com>
2016-12-16 17:19:15 +0800

    Add one

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,31 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
 var autoprefixer = require('gulp-autoprefixer');
+var connect = require('gulp-connect');
 var del = require('del');
 var livereload = require('gulp-livereload');
 
 gulp.task('clean', function() {
   return del(['css/*.css']);
 });
+gulp.task('webServer',function() {
+  connect.server({
+      livereload: true
+    });
+})
 gulp.task('less',['clean'],function(){
   gulp.src('css/less/*.less')
       .pipe(less())
       .pipe(autoprefixer('last 2 versions'))
       .pipe(gulp.dest('css'))
-      .pipe(livereload());
+      .pipe(connect.reload());
 });
 gulp.task('html', function() {
     return gulp.src('*.html')
         .pipe(gulp.dest(''))
-        .pipe(livereload())
+        .pipe(connect.reload());
 });
-gulp.task('default', ['clean','less']);
+gulp.task('default', ['less','webServer','watch']);
 gulp.task('watch', function () {
-    livereload.listen();
     gulp.watch(['css/less/*.less','index.html'], ['less','html']);
 });
